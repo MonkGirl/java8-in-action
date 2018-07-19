58e31ba91ff5a2c0bc3dece1cd9d12590f5aa238
MonkGirl <monkgirl2014@gmial.com>
2018-07-19 14:49:00 +0800

    new Date Time API

--- Java8InAction/src/com/monkgirl/java8InAction/chapter12/Exercise1.java
+++ Java8InAction/src/com/monkgirl/java8InAction/chapter12/Exercise1.java
@@ -9,11 +9,22 @@ import java.time.Instant;
 import java.time.Duration;
 import java.time.Period;
 import java.time.temporal.ChronoUnit;
+import java.time.DayOfWeek;
+import java.time.temporal.Temporal;
+import java.time.temporal.TemporalAdjuster;
+import java.time.temporal.TemporalAdjusters;
+import java.time.format.DateTimeFormatter;
+import java.util.TimeZone;
+import java.time.ZoneId;
+import static java.time.temporal.TemporalAdjusters.*;
 
 public class Exercise1{
     public static void main(String...args){
 	//run1();
-	run2();
+	//run2();
+	//run3();
+	//run4();
+	run5();
     }
 
     public static void run1(){
@@ -87,5 +98,101 @@ public class Exercise1{
 	System.out.println(fiveDays);
 	Period oneYearTwoMonthThreeDay = Period.of(1,2,3);
 	System.out.println(oneYearTwoMonthThreeDay);
+    }
+
+    public static void run3(){
+	LocalDate date1 = LocalDate.of(2014,3,26);
+	LocalDate date2 = LocalDate.now();
+
+	Period period = Period.between(date1, date2);
+	System.out.println(period.getDays());
+	System.out.println(Period.between(date1, date2).getMonths());
+
+	Period days = date1.until(date2);
+	System.out.println(days.getDays());
+	long allDays = date1.until(date2, ChronoUnit.DAYS);
+	System.out.println(allDays);
+
+	Period from = Period.from(period);
+	System.out.println(from.getDays());
+    }
+
+    public static void run4(){
+	LocalDate date = LocalDate.now();
+	System.out.println(date.withYear(2011));
+	System.out.println(date.withDayOfMonth(30));
+	System.out.println(date.with(ChronoField.MONTH_OF_YEAR, 2));
+
+	System.out.println(date.minusYears(7));
+	System.out.println(date.plusDays(13));
+	System.out.println(date.plus(20, ChronoUnit.MONTHS));
+
+	System.out.println(date.with(nextOrSame(DayOfWeek.SATURDAY)));
+	System.out.println(date.with(lastDayOfMonth()));
+	System.out.println(date.with(firstDayOfMonth()));
+	System.out.println(date.with(dayOfWeekInMonth(5, DayOfWeek.FRIDAY)));
+
+	System.out.println(new NextWorkingDay().adjustInto(date));
+
+	System.out.println(date.with(temporal -> {
+		    DayOfWeek dow = DayOfWeek.of(temporal.get(ChronoField.DAY_OF_WEEK));
+		    int dayToAdd = 1;
+		    if(dow == DayOfWeek.FRIDAY){
+			dayToAdd = 3;
+		    }else if(dow == DayOfWeek.SATURDAY){
+			dayToAdd = 2;
+		    }
+		    return temporal.plus(dayToAdd, ChronoUnit.DAYS);
+		}));
+
+	TemporalAdjuster nextWorkingDay = TemporalAdjusters
+	    .ofDateAdjuster(temporal -> {
+		    DayOfWeek dow = DayOfWeek.of(temporal.get(ChronoField.DAY_OF_WEEK));
+		    int dayToAdd = 1;
+		    if(dow == DayOfWeek.FRIDAY){
+			dayToAdd = 3;
+		    }else if(dow == DayOfWeek.SATURDAY){
+			dayToAdd = 2;
+		    }
+		    return temporal.plus(dayToAdd, ChronoUnit.DAYS);
+		});
+	System.out.println(date.with(nextWorkingDay));
+    }
+
+    public static void run5(){
+	LocalDate date = LocalDate.of(2017,10,11);
+	System.out.println(date.format(DateTimeFormatter.BASIC_ISO_DATE));
+	System.out.println(date.format(DateTimeFormatter.ISO_LOCAL_DATE));
+
+	System.out.println(LocalDate.parse("20150809", DateTimeFormatter.BASIC_ISO_DATE));
+
+	DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy|MM|dd");
+	System.out.println(date.format(formatter));
+
+	ZoneId zoneID = TimeZone.getDefault().toZoneId();
+	System.out.println(zoneID);
+	System.out.println(date.atStartOfDay(zoneID));
+
+	LocalDateTime time = LocalDateTime.now();
+	System.out.println(time.atZone(zoneID));
+
+	Instant instant = Instant.now();
+	System.out.println(instant.atZone(zoneID));
+
+	System.out.println(time.toInstant(zoneID));
+	System.out.println(LocalDateTime.ofInstant(instant, zoneID));
+    }
+}
+
+class NextWorkingDay implements TemporalAdjuster{
+    public Temporal adjustInto(Temporal temporal){
+	DayOfWeek dow = DayOfWeek.of(temporal.get(ChronoField.DAY_OF_WEEK));
+	int dayToAdd = 1;
+	if(dow == DayOfWeek.FRIDAY){
+	    dayToAdd = 3;
+	}else if(dow == DayOfWeek.SATURDAY){
+	    dayToAdd = 2;
+	}
+	return temporal.plus(dayToAdd, ChronoUnit.DAYS);
     }
 }
