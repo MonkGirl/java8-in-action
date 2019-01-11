7f950f71a05b0c97d19d822d172c91911d2bafc8
monkgirl2014@gmail.com <monkgirl2014@gmial.com>
2019-01-11 20:20:43 +0800

    chapter11 complete

---
+++ Java8InAction/src/com/monkgirl/java8InAction/chapter11/Discount.java
@@ -0,0 +1,42 @@
+
+package com.monkgirl.java8InAction.chapter11;
+
+import java.math.BigDecimal;
+import com.monkgirl.java8InAction.chapter11.Discount.Code;
+import java.util.concurrent.TimeUnit;
+
+public class Discount{
+    public enum Code{
+	NONE(0), SILVER(5), GOLD(10), PLATINUM(15), DIAMOND(20);
+
+	private final int percentage;
+
+	Code(int percentage){
+	    this.percentage = percentage;
+	}
+    }
+
+    public static String applyDiscount(Quote quote){
+	return quote.getShopName() + " price is " +
+	    Discount.apply(quote.getPrice(), quote.getDiscountCode());
+    }
+
+    private static double apply(double price, Code code){
+	delay();
+	return format(price * (100 - code.percentage) / 100);
+    }
+
+    private static void delay(){
+	try{
+	    TimeUnit.SECONDS.sleep(5);
+	}catch(Exception e){
+	    
+	}
+    }
+
+    public static double format(double d){
+	BigDecimal bd = BigDecimal.valueOf(d);
+	return bd.setScale(2, bd.ROUND_HALF_UP).doubleValue();
+    }
+
+}
---
+++ Java8InAction/src/com/monkgirl/java8InAction/chapter11/Shop.java
@@ -0,0 +1,52 @@
+package com.monkgirl.java8InAction.chapter11;
+
+import java.util.Random;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Future;
+
+public class Shop{
+    private String name;
+
+    public Shop(String name){
+	this.name = name;
+    }
+
+    public String getName(){
+	return name;
+    }
+    
+    public String getPrice(String product){
+	///return calculatePrice(product);
+	double price = calculatePrice(product);
+	Discount.Code code = Discount.Code.values()[new Random().nextInt(Discount.Code.values().length)];
+	return String.format("%s:%.2f:%s", name, price, code);
+    }
+
+    private double calculatePrice(String product){
+	delay();
+	//throw new RuntimeException("Not Found");
+	return new Random().nextDouble() * product.charAt(0) + product.charAt(1);
+    }
+
+    public void delay(){
+	try{
+	    Thread.sleep(1000L);
+	}catch(InterruptedException ie){
+	    throw new RuntimeException(ie);
+	}
+    }
+
+    public Future<Double> getPriceAsync(String product){
+	CompletableFuture<Double> futurePrice = new CompletableFuture<>();
+	/*new Thread(()->{
+		try{
+		    double price = calculatePrice(product);
+		    futurePrice.complete(price);
+		}catch(Exception e){
+		    futurePrice.completeExceptionally(e);
+		}
+		}).start();
+		return futurePrice;*/
+	return CompletableFuture.supplyAsync(() -> calculatePrice(product));
+    }
+}
